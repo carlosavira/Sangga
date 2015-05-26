9d737866cbff80361cb9d311a5e8dff783ce1d8a
carlosavira <carlosyaku@gmail.com>
2015-05-26 12:06:27 -0700

    Hello git,bourbon and jade! welcome to my crib:

---
+++ runJade.bat
@@ -0,0 +1,9 @@
+START
+
+:: npm install -g jade
+:: add %USERPROFILE%\Application Data\npm\node_modules (Windows XP), %AppData%\npm\node_modules (Windows 7) in system var
+:: type jade --help for checking
+:: locate file that has .jade extension
+:: 'jade filename.jade' to render filename.html
+:: 'jade -P filename.jade ' for pretty rendering 
+:: 'jade -w -P filename.jade' for watch
